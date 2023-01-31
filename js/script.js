99fdf475fd770bf9931927dcc2ad2bc7d8cfd327
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-01-31 22:42:49 +0100

    Add calling to function printMessage with text in variables.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let computerMove = `kamień`;
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let playerMove = 'papier';
+
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
