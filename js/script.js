c5152395abd27b6d35f738431614ad8befe0ace7
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-02-01 16:15:08 +0100

    Add logic for computerMove

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
-let computerMove = `kamień`;
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+console.log('Wylosowana liczba to: ' + randomNumber);
 
-let playerMove = 'papier';
+let computerMove = 'nieznany ruch';
 
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if (randomNumber == 2) {
+    computerMove = 'papier';
+} else {
+    computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
