b479c909698eeeaee254fc200d0b241cca2d4b98
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-02-02 18:28:04 +0100

    Remove commented JS

--- js/script.js
+++ js/script.js
@@ -38,6 +38,7 @@ function displayResult(argComputerMove, argPlayerMove) {
 }
 
 function playGame(playerInput) {
+    
     clearMessages();
 
     // Computer move
@@ -48,65 +49,19 @@ function playGame(playerInput) {
 
     let computerMove = getMoveName(randomNumber);
 
-    /*
-    if(randomNumber == 1){
-    computerMove = 'kamień';
-    } else if (randomNumber == 2) {
-        computerMove = 'papier';
-    } else {
-        computerMove = 'nożyce';
-    }
-    */
-
     printMessage('Mój ruch to: ' + computerMove);
 
     // Player move
 
-    // let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
     console.log('Gracz wpisał: ' + playerInput);
 
    let playerMove = getMoveName(playerInput);
-
-    /*
-    if(playerInput == '1'){
-    playerMove = 'kamień';
-    } else if (playerInput == '2') {
-        playerMove = 'papier';
-    } else if (playerMove == '3') {
-        playerMove = 'nożyce';
-    } else {
-        playerMove = 'nieznany ruch';
-    }
-    */
 
     printMessage('Twój ruch to: ' + playerMove);
 
     // Game result
 
     displayResult(computerMove, playerMove);
-
-    /*
-    if (computerMove == playerMove) {
-        printMessage('Remis!');
-    } else if (computerMove == 'kamień' && playerMove == 'papier') {
-        printMessage('Ty wygrywasz!');
-    } else if (computerMove == 'kamień' && playerMove == 'nożyce') {
-        printMessage('Komputer wygrywa!');
-    } else if (computerMove == 'papier' && playerMove == 'kamień') {
-        printMessage('Komputer wygrywa!');
-    } else if (computerMove == 'papier' && playerMove == 'nożyce') {
-        printMessage('Ty wygrywasz!');
-    } else if (computerMove == 'nożyce' && playerMove == 'kamień') {
-        printMessage('Ty wygrywasz!');
-    } else if (computerMove == 'nożyce' && playerMove == 'papier') {
-        printMessage('Komputer wygrywa!');
-    } else if (playerMove == 'nieznany ruch') {
-        printMessage('Nieznany ruch gracza. Nieprawidłowy wynik gry.');
-    } else {
-        printMessage('Nieprawidłowy wynik gry.');
-    }
-    */
 }
 
 // Event Listeners
